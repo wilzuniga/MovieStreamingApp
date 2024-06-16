fa14b18079b3192a3bc06d01e222271ee0b6c751
Wilmer Zuniga <wilmerzuniga.ant@gmail.com>
2024-06-15 21:01:54 -0600

    Cambios en el activity principal, adicion de clases que manejan los objetos de las pelis. Delicate, Taylor Swift

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,6 +38,8 @@ dependencies {
     implementation(libs.activity)
     implementation(libs.constraintlayout)
     implementation(libs.glider)
+    implementation(libs.volley)
+    implementation(libs.gson)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
--- app/src/main/java/com/example/myapplication/Dominio/SliderItems.java
+++ app/src/main/java/com/example/myapplication/Dominio/SliderItems.java
@@ -1,20 +1,7 @@
 package com.example.myapplication.Dominio;
 
-import com.example.myapplication.Clases.Movies;
-
 public class SliderItems {
     private int image;
-    private Movies movie;
-
-    public SliderItems(int image, Movies movie) {
-        this.image = image;
-        this.movie = movie;
-    }
-
-    public SliderItems(Movies movie) {
-
-    this.movie = movie;
-    }
 
 
     public SliderItems(int image) {
