f8e530a94f60d3665c28a7ad22905cfb794809ce
Wilmer Zuniga <wilmerzuniga.ant@gmail.com>
2024-06-20 07:20:39 -0600

    Cambios

--- app/src/main/java/com/example/myapplication/Activities/ActivityVaria.java
+++ app/src/main/java/com/example/myapplication/Activities/ActivityVaria.java
@@ -3,15 +3,52 @@ package com.example.myapplication.Activities;
 import android.os.Bundle;
 import android.view.View;
 
+
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
+import android.os.Handler;
+import android.util.Log;
+import android.view.View;
+import android.widget.ProgressBar;
+
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.viewpager2.widget.CompositePageTransformer;
+import androidx.viewpager2.widget.MarginPageTransformer;
+import androidx.viewpager2.widget.ViewPager2;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.example.myapplication.Adaptadores.CategoryListAdapter;
+import com.example.myapplication.Adaptadores.FilmListAdapter;
+import com.example.myapplication.Adaptadores.SliderAdapters;
+import com.example.myapplication.Dominio.GenresItem;
+import com.example.myapplication.Dominio.ListFilm;
+import com.example.myapplication.Dominio.SliderItems;
 
 import com.example.myapplication.R;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
+import java.util.ArrayList;
+import java.util.List;
 public class ActivityVaria extends AppCompatActivity {
+
+    private RecyclerView.Adapter adapterSearchedMovies;
+    private RecyclerView recyclerViewSearchedMovies;
+    private RequestQueue mRequestQueue;
+    private StringRequest mStringRequest;
+    private ProgressBar loading;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +60,36 @@ public class ActivityVaria extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        initView();
+    }
+
+    private void sendRequestSearchMovie(){
+        mRequestQueue = Volley.newRequestQueue(this);
+        loading.setVisibility(View.VISIBLE);
+        mStringRequest = new StringRequest(Request.Method.GET, "https://moviesapi.ir/api/v1/movies?page=2" + , new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                loading.setVisibility(View.GONE);
+                Gson gson = new Gson();
+                ListFilm listFilm = gson.fromJson(response, ListFilm.class);
+                List<ListFilm.ResultsItem> results = listFilm.getResults();
+                adapterSearchedMovies = new FilmListAdapter(results, ActivityVaria.this);
+                recyclerViewSearchedMovies.setAdapter(adapterSearchedMovies);
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d("Error.Response", error.toString());
+            }
+        });
+        mRequestQueue.add(mStringRequest);
+    }
+    private void initView() {
+        recyclerViewSearchedMovies = findViewById(R.id.recyclerViewSearchedMovies);
+        recyclerViewSearchedMovies.setHasFixedSize(true);
+        recyclerViewSearchedMovies.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
+        loading = findViewById(R.id.progressBar1);
     }
 
     @Override
